b3dfb13095679bb5afa4e05c3bdbf100b3904720
Justin Ehrenhofer <12520755+SamsungGalaxyPlayer@users.noreply.github.com>
2025-09-12 09:37:09 -0500

    Shuffle nodes and use onion
    Secure random is overkill but better safe than sorry!

--- lib/util/height.dart
+++ lib/util/height.dart
@@ -1,15 +1,19 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:math';
 import 'package:monero_light_wallet/util/socks_http.dart';
 
 import 'package:monero_light_wallet/services/tor_service.dart';
 
 Future<int> getCurrentBlockchainHeight() async {
   final urls = [
-    'https://xmr-node.cakewallet.com:18081/get_height',
-    'https://node.sethforprivacy.com:18089/get_height',
-    'https://node3.monerodevs.org:18089/get_height',
+    'cakexmrl7bonq7ovjka5kuwuyd3f7qnkz6z6s6dmsy3uckwra7bvggyd.onion:18081/get_height', // Cake Wallet
+    'rucknium757bokwv3ss35ftgc3gzb7hgbvvglbg3hisp7tsj2fkd2nyd.onion:18081/get_height', // Rucknium
+    'a6orjo6aiotog3njppja5jwnd3rexzfjiejxnojvw74p3kma45fundid.onion:18089/get_height', // StormyCloud
+    'un4yrhwq4d53caoiaadeiur5e5wgkgp74zw3p3twqh3nxh6ztz347dad.onion:18081/get_height,' // Triplebit
   ];
+
+  urls.shuffle(Random.secure());
 
   final proxyInfo = TorService.sharedInstance.getProxyInfo();
 

bb210d4edbbd7172671c9fdfa73c6b5e13ea537d
Justin Ehrenhofer <12520755+SamsungGalaxyPlayer@users.noreply.github.com>
2025-09-15 08:26:20 -0500

    Update nodes

--- lib/util/height.dart
+++ lib/util/height.dart
@@ -7,10 +7,9 @@ import 'package:monero_light_wallet/services/tor_service.dart';
 
 Future<int> getCurrentBlockchainHeight() async {
   final urls = [
-    'cakexmrl7bonq7ovjka5kuwuyd3f7qnkz6z6s6dmsy3uckwra7bvggyd.onion:18081/get_height', // Cake Wallet
     'rucknium757bokwv3ss35ftgc3gzb7hgbvvglbg3hisp7tsj2fkd2nyd.onion:18081/get_height', // Rucknium
-    'a6orjo6aiotog3njppja5jwnd3rexzfjiejxnojvw74p3kma45fundid.onion:18089/get_height', // StormyCloud
     'un4yrhwq4d53caoiaadeiur5e5wgkgp74zw3p3twqh3nxh6ztz347dad.onion:18081/get_height,' // Triplebit
+    'fz2lbxvjob6ifeonngaep2xvf2ypxjjn23i3ncblcxjreovev56ubyyd.onion:18089/get_height', // Unredacted
   ];
 
   urls.shuffle(Random.secure());

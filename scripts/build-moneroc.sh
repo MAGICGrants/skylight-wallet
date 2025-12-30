f717dd42a6b8188a63013b3a08a96a35d0384220
Keeqler <33733651+Keeqler@users.noreply.github.com>
2025-12-30 10:51:29 -0300

    Add monero_c build workflow

---
+++ scripts/build-moneroc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+apt update
+apt upgrade -y
+apt install -y build-essential pkg-config autoconf libtool ccache make cmake gcc g++ git curl \
+  lbzip2 libtinfo5 gperf unzip python-is-python3 gcc-mingw-w64-x86-64 g++-mingw-w64-x86-64
+
+git config --global --add safe.directory '*'
+git config --global user.email "info@magicgrants.org"
+git config --global user.name "MAGIC Grants"
+
+git clone https://github.com/vtnerd/monero_c.git
+cd monero_c
+git checkout lwsf
+git submodule update --init
+./apply_patches.sh monero
+./build_single.sh monero $TARGET_ARCH -j$(nproc)

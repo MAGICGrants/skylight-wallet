c6745ffcbd61f3e49796f2bbbc43ce83a14e77e0
Keeqler <33733651+Keeqler@users.noreply.github.com>
2026-01-29 16:00:23 -0300

    Add monero_c build job

--- scripts/build-moneroc.sh
+++ scripts/build-moneroc.sh
@@ -6,6 +6,9 @@ apt upgrade -y
 apt install -y build-essential pkg-config autoconf libtool ccache make cmake gcc g++ git curl \
   lbzip2 libtinfo5 gperf unzip python-is-python3 llvm gcc-mingw-w64-x86-64 g++-mingw-w64-x86-64
 
+update-alternatives --set x86_64-w64-mingw32-gcc /usr/bin/x86_64-w64-mingw32-gcc-posix
+update-alternatives --set x86_64-w64-mingw32-g++ /usr/bin/x86_64-w64-mingw32-g++-posix
+
 git config --global --add safe.directory '*'
 git config --global user.email "info@magicgrants.org"
 git config --global user.name "MAGIC Grants"

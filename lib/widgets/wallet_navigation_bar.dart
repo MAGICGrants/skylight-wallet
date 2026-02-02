c44019f30e6a4a0dbc2cb08977697c13f9591acb
Keeqler <33733651+Keeqler@users.noreply.github.com>
2026-02-02 16:30:51 -0300

    Prevent navigation bar from transitioning along with entire screen

--- lib/widgets/wallet_navigation_bar.dart
+++ lib/widgets/wallet_navigation_bar.dart
@@ -10,18 +10,21 @@ class WalletNavigationBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final i18n = AppLocalizations.of(context)!;
 
-    return NavigationBar(
-      selectedIndex: selectedIndex,
-      onDestinationSelected: (index) => {
-        if (index == 0 && selectedIndex != 0) {Navigator.pushNamed(context, '/wallet_home')},
-        if (index == 1 && selectedIndex != 1) {Navigator.pushNamed(context, '/address_book')},
-        if (index == 2 && selectedIndex != 2) {Navigator.pushNamed(context, '/settings')},
-      },
-      destinations: [
-        NavigationDestination(icon: Icon(Icons.wallet), label: i18n.navigationBarWallet),
-        NavigationDestination(icon: Icon(Icons.contacts), label: i18n.addressBookTitle),
-        NavigationDestination(icon: Icon(Icons.settings), label: i18n.navigationBarSettings),
-      ],
+    return Hero(
+      tag: 'main-navigation-bar',
+      child: NavigationBar(
+        selectedIndex: selectedIndex,
+        onDestinationSelected: (index) => {
+          if (index == 0 && selectedIndex != 0) {Navigator.pushNamed(context, '/wallet_home')},
+          if (index == 1 && selectedIndex != 1) {Navigator.pushNamed(context, '/address_book')},
+          if (index == 2 && selectedIndex != 2) {Navigator.pushNamed(context, '/settings')},
+        },
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.wallet), label: i18n.navigationBarWallet),
+          NavigationDestination(icon: Icon(Icons.contacts), label: i18n.addressBookTitle),
+          NavigationDestination(icon: Icon(Icons.settings), label: i18n.navigationBarSettings),
+        ],
+      ),
     );
   }
 }
